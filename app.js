f8e1fc4ff75b9980b53c90c66dcf031d212349a2
Yurong Tao <yurong.tao@autodesk.com>
2015-04-26 09:23:23 +0800

    user authentication

--- app.js
+++ app.js
@@ -1,31 +1,61 @@
 // DEBUG=LTWServer:* ./bin/www
+// passport tutorial https://scotch.io/tutorials/easy-node-authentication-setup-and-local
 
 var express = require('express');
-var path = require('path');
-// var favicon = require('serve-favicon');
-var logger = require('morgan');
+var app = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var configDB = require('./config/database.js');
 
-var app = express();
+
+// var path = require('path');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var routes = require('./routes/index');
+// var users = require('./routes/users');
+if (app.get('env') === 'production'){
+  mongoose.connect(configDB.url);
+}else {
+  mongoose.connect('mongodb://localhost/pretzel');
+}
+
+
+require('./config/passport')(passport);
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(logger('dev'));
 
-app.use('/', routes);
-app.use('/users', users);
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// routes ======================================================================
+require('./routes/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+// app.use('/', routes);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ config/database.js
@@ -0,0 +1,7 @@
+//https://scotch.io/tutorials/an-introduction-to-mongodb
+
+module.exports = {
+	'url' : 'mongodb://' + process.env.MONGOLAB_URI 
+    // 'url' : 'mongodb://localhost/pretzel' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
