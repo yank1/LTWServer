b693d9cee803537ab56dbc681b95aa750834306f
Yurong Tao <yurong.tao@autodesk.com>
2015-05-10 22:44:29 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /usr/src/app
 
 COPY . /usr/src/app
 
+RUN npm install
+
 EXPOSE 3000
 
 CMD [ "node" ,  "bin/www" ]
--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 // config/database.js
 module.exports = {
     'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': 'test123'
+        'host': process.env.MYSQL_PORT_3306_TCP_ADDR || 'localhost',
+        'user': process.env.MYSQL_USERNAME || 'root',
+        'password': process.env.MYSQL_PASSWORD || 'test123'
     },
-	'database': 'my_schema',
+	'database': process.env.MYSQL_INSTANCE_NAME || 'my_schema',
     'users_table': 'users'
 };
